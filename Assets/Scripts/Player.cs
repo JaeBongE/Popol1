54a0009d2829af283566e10a4a1f8fe4ec6ef048
JaeBongE <chlwogus04@gmail.com>
2023-12-21 02:22:24 +0900

    12.21 이동 및 애니메이션 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,18 +6,22 @@ public class Player : MonoBehaviour
 {
     [Header("플레이어 데이터")]
     Rigidbody2D rigid;
+    PolygonCollider2D polygonColider2D;
+    Animator anim;
     Vector3 moveDir;//default 0,0,0
     [SerializeField] float moveSpeed = 5.0f;
-
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        polygonColider2D = GetComponent<PolygonCollider2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         moving();
         turning();
+        doAnimation();
     }
     /// <summary>
     /// horizontal을 입력했을 때 플레이어 이동함수
@@ -39,5 +43,12 @@ public class Player : MonoBehaviour
             scale.x *= -1;
             transform.localScale = scale;
         }
+    }
+    /// <summary>
+    /// 애니메이션 변수 전달 함수
+    /// </summary>
+    private void doAnimation()
+    {
+        anim.SetInteger("Horizontal", (int)moveDir.x);
     }
 }
